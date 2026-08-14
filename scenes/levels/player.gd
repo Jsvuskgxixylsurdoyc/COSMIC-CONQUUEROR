4c8c4237fe2ba336159e54455cf03cdd1827c549
Jsvuskgxixylsurdoyc <115696764+Jsvuskgxixylsurdoyc@users.noreply.github.com>
2026-08-14 15:20:10 +0530

    fixed some bugs in the player position

--- scenes/levels/player.gd
+++ scenes/levels/player.gd
@@ -7,8 +7,7 @@ extends CharacterBody3D
 var movement_input := Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	movement_input = Input.get_vector("ui_down","ui_up","ui_left","ui_right")
+	movement_input = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
 	move_and_slide()
-	print(camera.global_rotation.y)
 	
