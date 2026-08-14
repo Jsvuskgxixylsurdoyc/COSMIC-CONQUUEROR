1d016e206506a444be96dfaa1d3b050f93648007
Jsvuskgxixylsurdoyc <115696764+Jsvuskgxixylsurdoyc@users.noreply.github.com>
2026-08-14 22:15:46 +0530

    i added the wall and the jump mechanics and made sure everything works perfectly

--- scenes/entities/player/cameracontroller.gd
+++ scenes/entities/player/cameracontroller.gd
@@ -1,12 +1,29 @@
 extends Node3D
 
+@export var min_limit_x: float = -1.5
+@export var max_limit_x: float = 1.5
+@export var horizontal_accelaration := 2.0
+@export var vertical_accelaration := 1.0
+@export var mouse_accelaration := 0.005
+
+
+
+func _process(delta: float) -> void:
+	var joy_dir = Input.get_vector("pan_left","pan_right","pan_up","pan_down")
+	rotate_from_vector(joy_dir * delta * Vector2(horizontal_accelaration, vertical_accelaration))
+	
+	
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotate_from_vector(event.relative * 0.005)
+		rotate_from_vector(event.relative * mouse_accelaration)
 
 func rotate_from_vector(v: Vector2):
 	if v.length() ==  0: return
-	rotation.y += v.x 
+	rotation.y -= v.x 
+	rotation.x -= v.y
+	rotation.x =clamp(rotation.x, min_limit_x, max_limit_x)
+	
 	
 	
 	
--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,11 +1,22 @@
 extends CharacterBody3D
 
+#jump
+@export var jump_height : float = 2.25
+
+
 @export var base_speed := 4.0
+
+@onready var camera = $Cameracontroller/Camera3D
 
 var movement_input := Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	movement_input = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
+	movement_input = Input.get_vector("ui_left","ui_right","ui_up","ui_down").rotated(-camera.global_rotation.y)
+	#velocity = Vector3(movement_input.x,0,movement_input.y) * base_speed
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = 40
+	velocity.y -= 0.5
+	
 	move_and_slide()
 	
