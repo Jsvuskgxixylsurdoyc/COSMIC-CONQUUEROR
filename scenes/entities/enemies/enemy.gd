8925dbcb9f2c8c569d7715b140fc767050e337b3
Jsvuskgxixylsurdoyc <115696764+Jsvuskgxixylsurdoyc@users.noreply.github.com>
2026-08-16 19:33:10 +0530

    i added the main boss and made him follow me but only upto a radius of 30 meters

---
+++ scenes/entities/enemies/enemy.gd
@@ -0,0 +1,19 @@
+class_name Enemy
+extends CharacterBody3D
+
+@onready var player = get_tree().get_first_node_in_group('Player')
+@onready var skin = get_node('skin')
+
+@export var walk_speed := 2.0
+@export var notice_radius := 30.0
+@export var attack_radius := 3.0
+
+
+func move_to_player(delta):
+	if position.distance_to(player.position) < notice_radius:
+		var target_dir = (player.position - position).normalized()
+		var target_vec2 = Vector2(target_dir.x, target_dir.z)
+		var target_angle = -target_vec2.angle() + PI/2
+		rotation.y = rotate_toward(rotation.y, target_angle, delta * 6.0)
+		velocity = Vector3(target_vec2.x, 0, target_vec2.y) * walk_speed
+		move_and_slide()
